cc3057cc60d6564649ef784b9993a5799dbb7fa9
dponce8 <daniel.ponce@outlook.com>
2025-08-31 17:44:28 -0300

    fix:quitar facturas

--- views/caja/movimiento-medio-lista.php
+++ views/caja/movimiento-medio-lista.php
@@ -32,37 +32,4 @@
     </table>
 </div>
 
-<?php if ($facturas != null) { ?>
-    <div style="padding-top: 20px; height : <?php if (count($facturas) < 3) {echo (60 + (count($facturas) * 35));} else {echo '130';}?>px; overflow : auto; white-space: nowrap; overflow-x : auto;">
-    <table class="table table-bordered">
-        <thead class="table-primary">
-        <tr style="font-size: 10px;">
-            <th style="font-size: 10px; font-weight: bold">Pto Vta</th>
-            <th style="font-size: 10px; font-weight: bold">Tipo</th>
-            <th style="font-size: 10px; font-weight: bold">Número</th>
-            <th style="font-size: 10px; font-weight: bold">Fecha Factura</th>
-            <th style="font-size: 10px; font-weight: bold">Importe</th>
-            <th style="font-size: 10px; font-weight: bold">Observaciones</th>
-            <th style="font-size: 10px; font-weight: bold">Usuario</th>
-            <th style="font-size: 10px; font-weight: bold">Creado</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php $c =1; foreach ($facturas as $m) { ?>
-            <tr class="table-light" >
-                <td><?= $m['id_punto']?></td>
-                <td><?= $m['tipo']?></td>
-                <td><?= $m['numero']?></td>
-                <td><?= date("d/m/Y",strtotime($m['fecha']))?></td>
-                <td><?= number_format($m['importe'], 2, ",", ".")?></td>
-                <td><?= $m['obs']?></td>
-                <td><?= $m['usuario']?></td>
-                <td><?= date("d/m/Y H:i",strtotime($m['creado']))?></td>
-            </tr>
-        <?php $c++; } ?>    
-        </tbody>
-    </table>
-</div>
-<?php } else { ?>
-    <div class="alert alert-warning">No hay facturas asociadas</div>
-<?php } ?>
+
